94db779f0ec645c8f664bc34074514bfb28a3cdf
natan-barel <natan.barel@outlook.co.il>
2023-12-06 12:52:43 +0200

    projects text changes

--- src/components/Projects/WorkCradData.js
+++ src/components/Projects/WorkCradData.js
@@ -4,14 +4,14 @@ import pro2 from "../../assets/prometheus-observability-stack-architecture-diagr
 const ProjectCardData = [
   {
     imgsrc: pro1,
-    title: "Terraform Project 1",
-    text: "Setup jenkins on Amazon-linux EC2 instance using Terraform and Ansible",
+    title: "AWS - Terraform Project 1",
+    text: "Setup jenkins on Amazon-linux EC2 instance (under VPC) using Terraform and Ansible",
     repo: "https://github.com/natan-barel/Terraform-Project-1/",
     view: "",
   },
   {
     imgsrc: pro2,
-    title: "Prometheus Observability Stack Using Docker",
+    title: "AWS - Prometheus Observability Stack Using Docker",
     text: "Prometheus, Grafana & Alert Manager using Terraform, Docker and Docker Compose",
     repo: "https://github.com/natan-barel/prometheus-observability-stack",
     view: "",
