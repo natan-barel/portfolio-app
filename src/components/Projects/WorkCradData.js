a66ec463e907126011dd32ef3eb722c9f116dde8
natan-barel <natan.barel@outlook.co.il>
2023-12-20 01:13:43 +0200

    added project number 4

--- src/components/Projects/WorkCradData.js
+++ src/components/Projects/WorkCradData.js
@@ -1,18 +1,19 @@
-import pro1 from "../../assets/tf-project-1-architecture-diagram.png";
-import pro2 from "../../assets/prometheus-observability-stack-architecture-diagram.png";
+import proj1 from "../../assets/tf-project-1-architecture-diagram.png";
+import proj2 from "../../assets/prometheus-observability-stack-architecture-diagram.png";
 import proj3 from "../../assets/Fault-Tolerant-Web-Hosting-on-Kubernetes.png";
+import proj4 from "../../assets/Single-Deployment-Using-Helm-and-K8s.png";
 
 
 const ProjectCardData = [
   {
-    imgsrc: pro1,
+    imgsrc: proj1,
     title: "AWS - Terraform Project 1",
     text: "Setup jenkins on Amazon-linux EC2 instance (under VPC) using Terraform and Ansible",
     repo: "https://github.com/natan-barel/Terraform-Project-1/",
     view: "",
   },
   {
-    imgsrc: pro2,
+    imgsrc: proj2,
     title: "AWS - Prometheus Observability Stack Using Docker",
     text: "Prometheus, Grafana & Alert Manager using Terraform, Docker and Docker Compose",
     repo: "https://github.com/natan-barel/prometheus-observability-stack",
@@ -23,6 +24,13 @@ const ProjectCardData = [
     title: "Fault-Tolerant Web Hosting on Kubernetes",
     text: "Containerize and deploy a fault-tolerant Web Application using Docker and Kubernetes",
     repo: "https://github.com/natan-barel/Fault-Tolerant-Web-Hosting-on-Kubernetes",
+    view: ""
+  },
+  {
+    imgsrc: proj4,
+    title: "Single Deployment Using Helm and K8s",
+    text: "Containerize and deploy a Web Application over a Kubernetes cluster using Helm chart",
+    repo: "https://github.com/natan-barel/Single-Deployment-Using-Helm-and-K8s",
     view: ""
   }
 ];
