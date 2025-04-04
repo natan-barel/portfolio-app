35499d43946ebb20fcdcf78ae8d84d61f4d975e4
natan-barel <natan.barel@outlook.co.il>
2025-04-04 20:34:17 +0300

    added certifications to home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import HeroImg from "../components/Images/HeroImg";
 import Work from "../components/Projects/Work";
 import AboutContent from "../components/About/AboutContent";
+import Certifications from "../components/Certifications/Certifications";
 
 const Home = () => {
   return (
     <div>
       <HeroImg />
       <AboutContent />
+      <Certifications />
       <Work />
     </div>
   );
