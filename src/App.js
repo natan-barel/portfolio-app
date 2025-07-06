4d714622f7fa8a5ba37e164b1a5403b21fc5624e
natan-barel <natan.barel@outlook.co.il>
2025-07-06 13:50:58 +0300

    remove articles route for now

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project" element={<Project />} />
-        <Route path="/articles" element={<ArticlesPage />} />
+        {/* <Route path="/articles" element={<ArticlesPage />} /> */}
         <Route path="/contact" element={<Contact />} />
         <Route path="/resume" element={<MyResume />} />
       </Routes>
